f5bc36db0eff964f0117d2c72e91b22da8723451
Miguel <miguel-sg01@hotmail.com>
2023-09-04 19:19:20 -0500

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('preline/plugin')],
+};
